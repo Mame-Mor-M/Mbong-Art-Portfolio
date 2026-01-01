b8933c73859b4acb05e67196509c8cd97c4e2438
MameMorM <mamemorm@my.yorku.ca>
2026-01-01 17:12:53 -0500

    React setup

---
+++ mbong-site/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './Hero.css';
+
+function App() {
+  return (
+    <div className="Hero">
+      <header className="Hero-header">
+        
+        <p>
+          Eat my shorts dummy
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
